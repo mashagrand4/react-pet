da26a94f675f7c01e3868c3eff097436a8aa7584
Maria Mashinskaya <maria.mashinskaya@vrpconsulting.com>
2019-12-11 17:28:04 +0300

    Addeed dev tools for redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,10 +3,11 @@ import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 import sagas from '../sagas'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(thunk, sagaMiddleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, sagaMiddleware)));
 
 sagaMiddleware.run(sagas);
 
